167462c49e6a968dada2cdea0f451f5f79df8c44
Matheus <matheus.dev5424@gmail.com>
2025-07-01 18:49:00 -0300

    As atividade inicia do curso primeiros passo com JavaScript

---
+++ Execicio de pratica3.js
@@ -0,0 +1,19 @@
+const Preco = 60;
+
+let formaPagamento = 4;
+
+if (formaPagamento === 1) {
+   let total = Preco - 0.1;
+   console.log(total);
+} else if (formaPagamento === 2) {
+   let total = Preco - 0.15;
+   console.log(total);
+} else if (formaPagamento === 3) {
+   let total = Preco - 0;
+   console.log(total);
+} else if (formaPagamento === 4) {
+   let total = Preco + 0.1;
+   console.log(total);
+} else {
+   console.log("Opção ivalida");
+}
---
+++ aula1 Variaveis e tipo de Operadores.js
@@ -0,0 +1,22 @@
+let Variavel1 = 20;
+
+console.log(Variavel1);
+
+Variavel1 = 30 + 50;
+
+console.log(Variavel1);
+
+/* Execicio  */
+
+const precoCombutivel = 5.59;
+
+let kmPorLitro = 10;
+
+let distancia = 100;
+
+const litroConsumidos = distancia / kmPorLitro;
+
+const valorGastos = litroConsumidos * precoCombutivel;
+
+console.log(litroConsumidos.toFixed(2));
+console.log(valorGastos.toFixed(2));
